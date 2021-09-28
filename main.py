f79988d1a0636d8d853895cf747b70640a7df98b
Bella Yang <hwy4012@gmail.com>
2021-09-28 11:36:42 -0400

    Initial commit for lesson1

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is the first Lession. 
+# My code prints Hello World 
+# String: 'Hello World'
+# Funtion: print()
+#Snakecase : Words that are delimited by an underscore 
+	# example: variable_x
+#Pascalcase: Words that are delimited by capital letters
+	# example VarableX
+#Camelcase: Words that are delimited by capital letters, excep the initial word
+	# example: VariableOneAndTwo
+
+# Types
+	# String
+	# Integer 
+	# Double 
+	# Float 
+	# List 
+	# Dictionary 
+	# Tuple 
+
+hello = 'hello'
+
+helloworld = 'Hello World'
+print(helloworld)
