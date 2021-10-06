197565d3fd61391212c3c7b11cafeebade6bac39
Bella Yang <hwy4012@gmail.com>
2021-10-06 00:42:58 +0000

    Lesson 2 Update

---
+++ Lesson2.py
@@ -0,0 +1,17 @@
+# Introduce yourself using print statement
+print('Hello, my name is Ray Choi, and my favorite fantasy genre is futuristic fantasy. I like beautiful stuff.')
+
+x="Hello World"
+print(x)
+
+# 1. add two numbers 
+
+# 2. minus two numbers 
+
+# 3. divide two numbers
+
+# 4. multiply two numbers 
+
+
+# 5. add up all the results from questions 1~4 and print out the result.
+
--- main.py
+++ main.py
@@ -17,8 +17,45 @@
 	# List 
 	# Dictionary 
 	# Tuple 
+  # Sets
+  # Stack
+  # Queue
+  # Trees
+  # Linked Lists
+  # Graphs
+  # HashMaps 
 
-hello = 'hello'
 
-helloworld = 'Hello World'
-print(helloworld)
+# hello = 'hello'
+# helloworld = 'Hello World'
+# print(helloworld)
+
+# Integers are whole number. They can be negative or positive. 
+# print("This is an integer")
+# print(f"This is number {6}")
+# print("This is number " + str(6))
+# print("this is number 6")
+
+# Add
+print("Addition")
+result_add = 1 + 3
+print(result_add)
+
+# Subtract 
+print("Subtract")
+result_substract = 3 - 1
+print(result_substract)
+
+#Division 
+print("Division")
+result_div = 6/2
+print(result_div)
+
+#Multiplication 
+print("Multiplication")
+result_multi = 3*6
+print(result_multi)
+
+
+total_number = result_add + result_substract + result_div + result_multi
+print(total_number)
